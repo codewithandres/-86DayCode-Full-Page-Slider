6f43b5bf9dec50325ec1d1bd0ca6a8be1002b149
Dize dev <axiozweb2022@gmail.com>
2024-09-07 21:43:33 -0500

    se creo las funciones slide up y slide down

--- js/app.js
+++ js/app.js
@@ -42,3 +42,52 @@ let animaOptions = {
     ease: power4.easeinOut,
 };
 
+const slide = dir => {
+    if (!playing) {
+        playing = true;
+
+        if (current + dir > 0) current = images.length - 1;
+        else if (current + dir >= images.length) current = 0;
+        else current += dir;
+
+        // slide up Function
+        const up = (part, next) => {
+            part.appendChild(next);
+            gsap.to(part, { ...animaOptions, y: -window.innerHeight }).then(
+                () => {
+                    part.children.at(0).remove();
+                    gsap.to(part, { duration: 0, y: 0 });
+                }
+            );
+        };
+        // slide dowm function
+        const down = (part, next) => {
+            part.append(next);
+            gsap.to(part, { duration: 0, y: -window.innerHeight });
+
+            gsap.to(part, { ...animaOptions, y: 0 }).then(() => {
+                part.children.at(1).remove();
+                playing = false;
+            });
+        };
+        for (let p in parts) {
+            let part = parts[p];
+            let next = document.createElement('div');
+
+            next.className = 'section';
+
+            let img = document.createElement('img');
+            img.src = images[current];
+
+            next.appendChild(img);
+
+            if ((p - Math.max(0, dir)) % 2) {
+                down(part, next);
+            } else {
+                up(part, next);
+            }
+        }
+    }
+};
+
+setInterval(() => slide(1), 2000);
